f13b9204f6a16ff0a3366954bf969f17ed786024
Danila Bolshakov <danila1999777@gmail.com>
2023-11-20 20:52:22 +0300

    Initial commit project settings on the Master branch
    Made the project settings, updated the .gitignore file, launched the project without errors on the Master branch

---
+++ SwiftShape/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SwiftShape
+//
+//  Created by Danila Bolshakov on 20.11.2023.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
